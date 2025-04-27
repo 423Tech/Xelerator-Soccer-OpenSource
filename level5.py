2b1ed3bdbbb0269ee4c345985bf7172f20dcb2d6
XiaoliMEMZ <38344755+XiaoliMEMZ@users.noreply.github.com>
2025-04-27 22:37:28 +0800

    Update level5.py

--- level5.py
+++ level5.py
@@ -258,7 +258,7 @@ def LidarCache()->list[list[int],list[int]]:
     return 0
 
 def LidarDists():
-    iCompass = str(compass.read())
+    iCompass = int(str(compass.read()))
     sSentData = 'cmp'+str(iCompass)+'end'
     SendUART(1,sSentData)
     sReceivedDataFrame = GetUART(1)
