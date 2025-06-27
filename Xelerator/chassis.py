d330eef124ab3bf210316318ee01d74267812cd1
XiaoliMEMZ <38344755+XiaoliMEMZ@users.noreply.github.com>
2025-06-27 14:02:08 +0800

    Update

--- Xelerator/chassis.py
+++ Xelerator/chassis.py
@@ -1,18 +1,17 @@
+import math
+
 class Car:
-    def __init__(self,SetMotor,GetYaw=None):
-        self.SetMotor = SetMotor
+    def __init__(self,SetMotorFunc,GetYaw=None):
+        self.SetMotorFunc = SetMotorFunc
         self.GetYaw = GetYaw
 
-        self.Kp = 0.5
+        self.Kp = 1
     
-    # def SetMotor(self,Speed1,Speed2,Speed3,Speed4):
-    #     self.SetMotorFunc(int(Speed1), int(Speed2), int(Speed3), int(Speed4))
+    def SetMotor(self,Speed1,Speed2,Speed3,Speed4):
+        self.SetMotorFunc(int(Speed1), int(Speed2), int(Speed3), int(Speed4))
     
     def SetKp(self,Kp):
         self.Kp = Kp
-    
-    # def GetYaw(self):
-    #     return self.GetYawFunc()
     
     def Go(self,SpeedX,SpeedY,SpeedZ):
         Speed1 = SpeedX + SpeedY + SpeedZ
@@ -20,6 +19,15 @@ class Car:
         Speed3 = SpeedY - SpeedX - SpeedZ
         Speed4 = SpeedX + SpeedY - SpeedZ
         self.SetMotor(Speed1, Speed2, Speed3, Speed4)
+    
+    def GoA(self,FacingAngle,MovingAngle,Speed):
+        if self.GetYaw is None:
+            return False
+        Yaw = self.GetYaw()
+        rad = math.radians(MovingAngle+360-Yaw)
+        SpeedX = int(math.sin(rad) * Speed)
+        SpeedY = int(math.cos(rad) * Speed)
+        self.GoV(SpeedX,SpeedY,FacingAngle)
 
     def GoV(self,SpeedX,SpeedY,FacingAngle):
         if self.GetYaw is None:
@@ -29,10 +37,6 @@ class Car:
         Error = (Error + 180) % 360 - 180  # Normalize to [-180, 180]
         SpeedZ = - Error * self.Kp
         self.Go(SpeedX, SpeedY, SpeedZ)
-    
-    def GoA(self,FacingAngle,MovingAngle,Speed):
-        if self.GetYaw is None:
-            return False
     
     def GoX(self,Angle,Speed):
         if self.GetYaw is None:
