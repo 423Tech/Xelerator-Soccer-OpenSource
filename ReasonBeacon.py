75907c9582ad0c0d49c69065bc8d3c240dc7a00b
Oiictealus <ctgzs0811@163.com>
2025-07-09 21:53:07 +0800

    update RB

--- ReasonBeacon.py
+++ ReasonBeacon.py
@@ -38,8 +38,8 @@ class BTBeacon:
         self.CreateConnection()
         
     def StartServer(self):
-        """启动蓝牙服务器"""
-        while True:
+            """启动蓝牙服务器"""
+        # while True:
             try:
                 # 创建蓝牙套接字
                 self.logger.info(f"蓝牙服务器启动，监听端口: {self.port}")
@@ -52,7 +52,7 @@ class BTBeacon:
                 receive_thread = threading.Thread(target=self.receive_messages)
                 receive_thread.daemon = True
                 receive_thread.start()
-                break
+                # break
             except bluetooth.btcommon.BluetoothError as e:
                 self.connected = False
             except Exception as e:
