fea244001d4720a571e7c68072c02812dc4f40d5
423Tech <zhengsicheng081226@icloud.com>
2025-07-10 21:47:56 +0800

    1234

--- Defence_link.py
+++ Defence_link.py
@@ -1,5 +1,6 @@
 from kits import *
 def DefenceLink():#"0":[75,95],"1":[35,85]
+    time.sleep(0.03)
     lBallPos = GetBallPos()
     iBX,iBY = lBallPos[0],lBallPos[1]
     Compass = compass()
@@ -10,20 +11,23 @@ def DefenceLink():#"0":[75,95],"1":[35,85]
     # GOAL_POS = [0, -100]
     if [bX,bY] == [1024,1024]:
         defend_x = 0
-        defend_y = -80
+        defend_y = -85
     else:
-        ABSx = bX / (95+bY) * 10
+        if bY == -95:
+            ABSx = bX  * 10
+        else:
+            ABSx = bX / (95+bY) * 10
         print(ABSx)
         if abs(ABSx) <= 35:
             defend_x = ABSx
-            defend_y = -80
+            defend_y = -85
         elif abs(ABSx) > 35 and abs(ABSx) < 42:
             defend_x = ABSx
-            defend_y = -75 - (abs(ABSx)-35)
+            defend_y = -85 - (abs(ABSx)-35)
         else:
             if ABSx > 0:
                 defend_x = 42
             else:
                 defend_x = -42
-            defend_y = -95 + (95+bY)/bX * 45
-    Pos2Pos([defend_x, defend_y, Fangle], False)
+            defend_y = -95 + (85+bY)/abs(bX) * 45
+    Pos2Pos([defend_x, defend_y, Fangle], False,80)
