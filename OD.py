30a063a24b82ba00713feb69ded5d2a0b17af5cf
XiaoliMEMZ <38344755+XiaoliMEMZ@users.noreply.github.com>
2025-07-13 16:03:53 +0800

    Update OD.py

--- OD.py
+++ OD.py
@@ -1,104 +1,130 @@
 from kits import *
-def DefenceLink(x):#"0":[75,95],"1":[35,85]
-    #Comlpeted
-    time.sleep(0.03)
-    lBallPos = GetBallPos()
-    iBX,iBY = lBallPos[0],lBallPos[1]
-    Compass = compass()
-    Angle = (math.degrees(math.atan2(iBX, iBY)) + 360) % 360 
-    Fangle = Angle + Compass
-    bX, bY = AbsBallPos()
-    logger.debug("bX"+str(bX)+" bY"+str(bY))
-    IX = GetPos()[0]
-    IY = GetPos()[1]
-    if x == 1:
-        defend_x = 0
-        defend_y = -80
-        if [bX,bY] == [1024,1024]: #没有扫描到球 那就锁定车辆位置
-            defend_x = 0
-            defend_y = -80
-            Cx = CEnemyPos()[0]
-            Cy = CEnemyPos()[1]
-            defend_y = 0
-            Fangle =(-int(math.degrees(math.atan2(Cy,Cx)) - 90)+ compass())%360
-            CDistance = math.sqrt(Cx**2 + Cy**2)
-            ChassisX = CDistance * math.sin(math.radians(Fangle))+IX
-            ChassisY = CDistance * math.cos(math.radians(Fangle))+IY
+from Defence_Back import *
+from Defence_Follow_panning import *
+from Defence_Follow_ratio import *
+from Defence_link import *
+from Offence_Back import *
+# ArisuCam = ArisuIntelligence()
 
-            logger.debug("CX"+str(ChassisX)+" CY"+str(ChassisY))
+#Math Mod
+###################################################################################################
 
-            peripheral.StopDribble()
-            if ChassisY == -95 :
-                ABSx = ChassisX  * 10
-            elif ChassisY < -80:
-                ABSx = ChassisX 
-            else:
-                ABSx = ChassisX / (95+ChassisY) * 15
-            
-            if abs(ABSx) <= 35:
-                defend_x = ABSx
-                defend_y = -80
-            elif abs(ABSx) > 35 and abs(ABSx) < 42:
-                defend_x = ABSx
-                defend_y = -80 - (abs(ABSx)-35)
-            else:
-                if ChassisX > 0:
-                    defend_x = 42
-                else:
-                    defend_x = -42
-                defend_y = -95 + (80+ChassisY)/abs(ChassisX) * 45
-        else: #有球锁球
-            peripheral.DribbleBall()
-            if bY == -95:
-                ABSx = bX  * 10
-            else:
-                ABSx = bX / (95+bY) * 10
-            print(ABSx)
-            if abs(ABSx) <= 35:
-                defend_x = ABSx
-                defend_y = -80
-            elif abs(ABSx) > 35 and abs(ABSx) < 42:
-                defend_x = ABSx
-                defend_y = -80 - (abs(ABSx)-35)
-            else:
-                if bX > 0:
-                    defend_x = 42
-                else:
-                    defend_x = -42
-                defend_y = -95 + (80+bY)/abs(bX) * 45
-                logger.debug("DX"+str(defend_x)+" DY"+str(defend_y))
+def OD(): 
+    iX,iY = GetPos()[0],GetPos()[1]
+    bx,by = AbsBallPos()
+    Cx = CEnemyPos()[0]
+    Cy = CEnemyPos()[1]
+    Fangle =(-int(math.degrees(math.atan2(Cy,Cx)) - 90)+ compass())%360
+    CDistance = math.sqrt(Cx**2 + Cy**2)
+    ChassisY = CDistance * math.cos(math.radians(Fangle))+iY
+    if abs(iX) <35 and abs(iY) <85: #防止出界限
+        chassis.SetMotor(100,100,100,100)
+    if abs(iX) > 75 : 
+        print(1)
+        if iX>0:
+            chassis.SetMotor(-100,100,100,-100)
+        else:
+            chassis.SetMotor(100,-100,-100,100)
+    elif abs(iY) > 95:
+        print(2)
+        if iY > 0:
+            chassis.SetMotor(-100,-100,-100,-100)
+        else:
+            chassis.SetMotor(100,100,100,100)
+
+
+
+
     else:
-        defend_x = 0
-        defend_y = -85
-        if [bX,bY] == [1024,1024]: 
-            peripheral.StopDribble()
-            defend_x = 0
-            defend_y = -85
-            Fangle = 0
-        else: 
-            peripheral.DribbleBall()
-            if bY == -95:
-                ABSx = bX  * 10
-            elif bY <= -85:
-                ABSx = bX
+        if PeerPosition == [1024,1024]:
+            if [bx,by] == [1024,1024]:
+                if ChassisY > 0 or CEnemyPos() == [1204,1204]: #对方车辆位于对方半场
+                    if PeerPosition[1]> iY :
+                        print(2)
+                        Pos2Pos([0,-40,0],False,100)
+                    else:
+                        Pos2Pos([0,-85,0],False,100)
+                else: #有可能是背身持球 没有扫描到球，锁车
+                    if ChassisY > -50:
+                        print(3.25)
+                        FollowPRatio(0)
+                    else:
+                        print(3.75)
+                        DefenceLink(1)
             else:
-                ABSx = bX / (95+bY) * 10
-            print(ABSx)
-            if abs(ABSx) <= 35:
-                defend_x = ABSx
-                defend_y = -85
-            elif abs(ABSx) > 35 and abs(ABSx) < 42:
-                defend_x = ABSx
-                defend_y = -85 - (abs(ABSx)-35)
-            else:
-                if bX > 0:
-                    defend_x = 42
+                if BallFlag == [1,0]:
+                    print(4)
+                    Offence()
                 else:
-                    defend_x = -42
-                defend_y = -95 + (85+bY)/abs(bX) * 45
+                    if bx > -50:
+                        print(8)
+                        FollowPRatio(0)
+                    else:
+                        print(9)
+                        DefenceLink(1)
+        else:
+            if [bx,by] == [1024,1024]:
+                if ChassisY > 0 or CEnemyPos() == [1204,1204]: #对方车辆位于对方半场
+                    if PeerPosition[1]> iY :
+                        print(2)
+                        Pos2Pos([0,-85,0],False,100)
+                    else:
+                        Pos2Pos([0,-40,0],False,100)
+                else: #有可能是背身持球 没有扫描到球，锁车
+                    if ChassisY > -50:
+                        print(3.25)
+                        FollowPRatio(1)
+                    else:
+                        print(3.75)
+                        DefenceLink(1)
+            else:
+                if by > 0:
+                    if BallFlag == [1,0]:
+                        print(4)
+                        Offence()
+                    elif BallFlag == [0,1]:
+                        print(5)
+                        FollowPRatio(0)
+                    else:
+                        print(6)
+                        NormalShoot(0)
+                else:
+                    if BallFlag == [1,0]:
+                        print(7)
+                        Offence()
+                    elif BallFlag == [0,1]:
+                        if bx > -50:
+                            print(8)
+                            FollowPRatio(0)
+                        else:
+                            print(9)
+                            DefenceLink(1)
+                    else:
+                        print(10)
+                        NormalShoot(0)
 
-    if abs(defend_x - IX) <10 and abs(defend_y - IY)<10:
-        chassis.stop()
-        chassis.GoZ(Fangle)
+def Offence():
+    iX = GetPos()[0]
+    iY = GetPos()[1]
+    if abs(iX) > 35:
+        OHMYBACK()
     else:
-        Pos2Pos([defend_x, defend_y, Fangle], False,200)
+        NormalShoot()
+
+def OffDenfence():
+    bx,by = AbsBallPos()
+    if [bx,by] == [1024,1024]:
+        DefenceBack()
+    else:
+        if [bx,by] == [1207,1207]:
+            Offence()
+        else:
+            if by > -20:
+                LockBallSlip()
+            else:
+                if bx > -50:
+                    FollowPRatio(0)
+                else:
+                    DefenceLink(0)
+
+
