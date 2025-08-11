cc8b9be93340edfafc59ee2da4ca662626a45f15
Suggestion <84503411+423Tech@users.noreply.github.com>
2025-08-11 09:09:26 +0800

    del sim

--- rcj-soccersim/controllers/rcj_soccer_ball/rcj_soccer_ball.py
+++
@@ -1,9 +0,0 @@
-from controller import Robot
-
-robot = Robot()
-ball_emitter = robot.getDevice("ball emitter")
-
-data = "x"  # Packet cannot be empty
-
-while robot.step(32) != -1:
-    ball_emitter.send(data)
