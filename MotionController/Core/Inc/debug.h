6a988fcaf212a285f65ce88e4e2ea2888b1a10d2
Chen Yehui <ChenYehuiCN@outlook.com>
2026-01-02 18:22:33 +0800

    写debug串口打印工具

--- MotionController/Core/Inc/debug.h
+++ MotionController/Core/Inc/debug.h
@@ -8,6 +8,13 @@
 #ifndef INC_DEBUG_H_
 #define INC_DEBUG_H_
 
+#define DEBUG_ENABLE (1)
 
+#if DEBUG_ENABLE
+    int debug_printf(const char *format, ...);
+    #define DEBUG_PRINTF(...) debug_printf(__VA_ARGS__)
+#else
+    #define DEBUG_PRINTF(...)
+#endif
 
 #endif /* INC_DEBUG_H_ */
--- MotionController/Core/Src/debug.c
+++ MotionController/Core/Src/debug.c
@@ -5,4 +5,29 @@
  *      Author: yehui
  */
 
+#include <stdarg.h>
+#include "stdio.h"
+#include "string.h"
+#include "main.h"
+#include "usart.h"
 
+#define DEBUG_BUFFER_SIZE (64)
+
+int debug_printf(const char *format, ...)
+{
+    char buffer[DEBUG_BUFFER_SIZE];
+    va_list args;
+    int len;
+
+    va_start(args, format);
+    len = vsnprintf(buffer, DEBUG_BUFFER_SIZE, format, args);
+    if (len >= DEBUG_BUFFER_SIZE) {
+        len = DEBUG_BUFFER_SIZE - 1;
+        buffer[len] = '\0';
+    }
+    va_end(args);
+
+    HAL_UART_Transmit(&huart4, (uint8_t *)buffer, len, HAL_MAX_DELAY);
+
+    return len;
+}
