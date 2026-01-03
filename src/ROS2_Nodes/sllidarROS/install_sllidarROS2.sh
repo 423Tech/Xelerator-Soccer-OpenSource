6ee3880260d03039d92abe9a194df5b7465a76f0
NeiKos496 <zsc_hcl@126.com>
2026-01-03 21:39:26 +0800

    update scripts

--- src/ROS2_Nodes/sllidarROS/install_sllidarROS2.sh
+++ src/ROS2_Nodes/sllidarROS/install_sllidarROS2.sh
@@ -1,8 +1,8 @@
 git clone https://github.com/Slamtec/sllidar_ros2.git
 
 source /opt/ros/jazzy/setup.bash
+pip install catkin_pkg
 colcon build --symlink-install
-source ./install/setup.bash
 
 echo "source ~/Xelerator-Soccer-OpenSource/src/ROS2_Nodes/sllidarROS/install/setup.bash" >> ~/.bashrc
 
