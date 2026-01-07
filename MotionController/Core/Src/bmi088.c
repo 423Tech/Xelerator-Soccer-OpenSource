62166760b64d972009a1535af6f7af87870ca835
Chen Yehui <ChenYehuiCN@outlook.com>
2026-01-07 23:43:44 +0800

    改协议：增加复合指令

--- MotionController/Core/Src/bmi088.c
+++ MotionController/Core/Src/bmi088.c
@@ -12,10 +12,8 @@
 
 #define TX_RX_BUFF_SIZE (6 + 1)
 
-static union {
-	uint8_t n[TX_RX_BUFF_SIZE];
-	volatile uint8_t v[TX_RX_BUFF_SIZE];
-} tx_buff, rx_buff;
+uint8_t tx_buff[TX_RX_BUFF_SIZE];
+uint8_t rx_buff[TX_RX_BUFF_SIZE];
 
 static int gyro_target_samples;
 static int gyro_calibrated_samples;
@@ -28,20 +26,20 @@ float bmi088_gyro_angle[3] = {0, 0, 0};
 
 static void bmi088_write_gyro(uint8_t reg, uint8_t data)
 {
-	tx_buff.n[0] = reg & 0x7F;
-	tx_buff.n[1] = data;
+	tx_buff[0] = reg & 0x7F;
+	tx_buff[1] = data;
 	HAL_GPIO_WritePin(GPIOD, GPIO_PIN_12, GPIO_PIN_RESET);
-	HAL_SPI_Transmit(&hspi2, tx_buff.n, 2, HAL_MAX_DELAY);
+	HAL_SPI_Transmit(&hspi2, tx_buff, 2, HAL_MAX_DELAY);
 	HAL_GPIO_WritePin(GPIOD, GPIO_PIN_12, GPIO_PIN_SET);
 	delay_us(2);
 }
 
 void bmi088_burst_read_gyro(uint8_t reg, int size)
 {
-	tx_buff.n[0] = reg | 0x80;
-	memset(tx_buff.n + 1, 0xFF, size);
+	tx_buff[0] = reg | 0x80;
+	memset(tx_buff + 1, 0xFF, size);
 	HAL_GPIO_WritePin(GPIOD, GPIO_PIN_12, GPIO_PIN_RESET);
-	HAL_SPI_TransmitReceive_DMA(&hspi2, tx_buff.n, rx_buff.n, size + 1);
+	HAL_SPI_TransmitReceive_DMA(&hspi2, tx_buff, rx_buff, size + 1);
 }
 
 void bmi088_init_gyro(void)
@@ -72,13 +70,13 @@ void bmi088_process_gyro_angle(void)
 
 	if (is_first_call) {
 		for (i = 0; i < 3; i++) {
-			int16_t raw = (int16_t)(rx_buff.v[2 * i + 2] << 8 | rx_buff.v[2 * i + 1]);
+			int16_t raw = (int16_t)(rx_buff[2 * i + 2] << 8 | rx_buff[2 * i + 1]);
 			last_rate[i] = ((float)raw - gyro_offset[i]) * (2000.0f / 32767.0f);
 		}
 		is_first_call = false;
 	} else {
 		for (i = 0; i < 3; i++) {
-			int16_t raw = (int16_t)(rx_buff.v[2 * i + 2] << 8 | rx_buff.v[2 * i + 1]);
+			int16_t raw = (int16_t)(rx_buff[2 * i + 2] << 8 | rx_buff[2 * i + 1]);
 			if (is_calibrating_gyro_offset && gyro_calibrated_samples < gyro_target_samples)
 				gyro_offset_sum[i] += raw;
 			rate[i] = ((float)raw - gyro_offset[i]) * (2000.0f / 32767.0f);
--- MotionController/Core/Src/protocol.c
+++ MotionController/Core/Src/protocol.c
@@ -5,6 +5,7 @@
  *      Author: yehui
  */
 
+#include <stdbool.h>
 #include <string.h>
 #include "usart.h"
 #include "bmi088.h"
@@ -14,48 +15,79 @@
 #define TX_MAX_LEN (32)
 #define RX_MAX_LEN (32)
 
-static union {
-	uint8_t n[TX_MAX_LEN];
-	volatile uint8_t v[TX_MAX_LEN];
-} tx_buff;
-
-static union {
-	uint8_t n[RX_MAX_LEN];
-	volatile uint8_t v[RX_MAX_LEN];
-} rx_buff[2];
-
-static int protocol_current_rx_buff_idx = 0;
+static uint8_t tx_buff[TX_MAX_LEN];
+static uint8_t rx_buff[2][RX_MAX_LEN];
+static int current_rx_buff_idx = 0;
 
 void protocol_start_receive_host(void)
 {
-	protocol_current_rx_buff_idx = protocol_current_rx_buff_idx == 0 ? 1 : 0;
-	HAL_UARTEx_ReceiveToIdle_DMA(&huart4, rx_buff[protocol_current_rx_buff_idx].n, RX_MAX_LEN);
+	current_rx_buff_idx ^= 1;
+	HAL_UARTEx_ReceiveToIdle_DMA(&huart4, rx_buff[current_rx_buff_idx], RX_MAX_LEN);
+}
+
+static void process_handshake(uint8_t *tx, uint8_t *rx, uint8_t **end_tx, uint8_t **end_rx)
+{
+	(void)rx;
+	tx[1] = is_calibrating_gyro_offset ? 0x01 : 0x00;
+	*end_tx += 1 + sizeof(uint8_t);
+	*end_rx += 1;
+}
+
+static void process_get_bmi088_data(uint8_t *tx, uint8_t *rx, uint8_t **end_tx, uint8_t **end_rx)
+{
+	(void)rx;
+	memcpy(tx + 1, bmi088_gyro_angle, sizeof(bmi088_gyro_angle));
+	memcpy(tx + 1 + sizeof(bmi088_gyro_angle), (void *)(&bmi088_drdy_timestamp), sizeof(bmi088_drdy_timestamp));
+	*end_tx += 1 + sizeof(bmi088_gyro_angle) + sizeof(bmi088_drdy_timestamp);
+	*end_rx += 1;
+}
+
+static void process_set_wheels_speed(uint8_t *tx, uint8_t *rx, uint8_t **end_tx, uint8_t **end_rx)
+{
+	(void)tx;
+	memcpy(motor_target_wheels_rpm, rx + 1, sizeof(motor_target_wheels_rpm));
+	*end_tx += 1;
+	*end_rx += 1 + sizeof(motor_target_wheels_rpm);
+}
+
+static void process_kick(uint8_t *tx, uint8_t *rx, uint8_t **end_tx, uint8_t **end_rx)
+{
+	(void)tx;
+	(void)rx;
+	kick();
+	*end_tx += 1;
+	*end_rx += 1;
 }
 
 void protocol_process_received_frame(void)
 {
-	tx_buff.n[0] = rx_buff[protocol_current_rx_buff_idx].v[0] | 0x80;
-	switch(rx_buff[protocol_current_rx_buff_idx].v[0]) {
-	case 0x01:
-		if (is_calibrating_gyro_offset)
-			tx_buff.n[1] = 0x01;
-		else
-			tx_buff.n[1] = 0x00;
-		HAL_UART_Transmit_IT(&huart4, tx_buff.n, 1 + 1);
-		break;
-	case 0x02:
-		memcpy(tx_buff.n + 1, bmi088_gyro_angle, sizeof(bmi088_gyro_angle));
-		memcpy(tx_buff.n + 1 + sizeof(bmi088_gyro_angle), (void *)(&bmi088_drdy_timestamp), sizeof(bmi088_drdy_timestamp));
-		HAL_UART_Transmit_IT(&huart4, tx_buff.n, 1 + sizeof(bmi088_gyro_angle) + sizeof(bmi088_drdy_timestamp));
-		break;
-	case 0x03:
-		memcpy(motor_target_wheels_rpm, rx_buff[protocol_current_rx_buff_idx].n + 1, sizeof(motor_target_wheels_rpm));
-		break;
-	case 0x04:
-		kick();
-		break;
-	default:
-		break;
-	}
+	uint8_t *tx_buff_ptr = tx_buff;
+	uint8_t *rx_buff_ptr = rx_buff[current_rx_buff_idx];
+	bool is_processing_composite_cmd = false;
+	do {
+		tx_buff_ptr[0] = rx_buff_ptr[0] | 0x80;
+		switch(rx_buff_ptr[0]) {
+		case 0x00:
+			is_processing_composite_cmd ^= true;
+			tx_buff_ptr++;
+			rx_buff_ptr++;
+			break;
+		case 0x01:
+			process_handshake(tx_buff_ptr, rx_buff_ptr, &tx_buff_ptr, &rx_buff_ptr);
+			break;
+		case 0x02:
+			process_get_bmi088_data(tx_buff_ptr, rx_buff_ptr, &tx_buff_ptr, &rx_buff_ptr);
+			break;
+		case 0x03:
+			process_set_wheels_speed(tx_buff_ptr, rx_buff_ptr, &tx_buff_ptr, &rx_buff_ptr);
+			break;
+		case 0x04:
+			process_kick(tx_buff_ptr, rx_buff_ptr, &tx_buff_ptr, &rx_buff_ptr);
+			break;
+		default:
+			break;
+		}
+	} while (is_processing_composite_cmd);
+	HAL_UART_Transmit_IT(&huart4, tx_buff, tx_buff_ptr - tx_buff);
 	protocol_start_receive_host();
 }
