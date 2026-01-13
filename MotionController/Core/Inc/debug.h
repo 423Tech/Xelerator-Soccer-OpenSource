f916f70c6150a25d71eb9836c80a932e43493e93
Chen Yehui <ChenYehuiCN@outlook.com>
2026-01-14 00:22:09 +0800

    修改缩进

--- MotionController/Core/Inc/debug.h
+++ MotionController/Core/Inc/debug.h
@@ -11,10 +11,10 @@
 #define DEBUG_ENABLE (1)
 
 #if DEBUG_ENABLE
-    int debug_printf(const char *format, ...);
-    #define DEBUG_PRINTF(...) debug_printf(__VA_ARGS__)
+	int debug_printf(const char *format, ...);
+	#define DEBUG_PRINTF(...) debug_printf(__VA_ARGS__)
 #else
-    #define DEBUG_PRINTF(...)
+	#define DEBUG_PRINTF(...)
 #endif
 
 #endif /* INC_DEBUG_H_ */
