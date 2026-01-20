aa3f5d9dee678a606bdbcd3d610ed60158ac160f
nArisu <k_423@icloud.com>
2026-01-20 14:54:23 +0800

    merge old commits

--- src/ROS2_Nodes/sllidarROS/install_sllidarROS2.sh
+++ src/ROS2_Nodes/sllidarROS/install_sllidarROS2.sh
@@ -1,6 +1,8 @@
 git clone git@github.com:Slamtec/sllidar_ros2.git
 
 source /opt/ros/jazzy/setup.bash
+sudo apt install -y colcon
+sudo apt install -y  ros-jazzy-rmw-fastrtps-cpp
 colcon build --symlink-install
 
 echo "source ~/Xelerator-Soccer-OpenSource/src/ROS2_Nodes/sllidarROS/install/setup.bash" >> ~/.bashrc
